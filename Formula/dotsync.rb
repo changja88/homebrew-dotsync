bc407413232441bb6e0180fa34c6a78ba0723aa1
hyun <changja00@gmail.com>
2026-06-25 15:32:17 +0900

    chore: bump version to 0.1.27

--- Formula/dotsync.rb
+++ Formula/dotsync.rb
@@ -1,7 +1,7 @@
 class Dotsync < Formula
   desc "Sync app configs with a local folder"
   homepage "https://github.com/changja88/homebrew-dotsync"
-  url "https://github.com/changja88/homebrew-dotsync/archive/refs/tags/v0.1.26.tar.gz"
+  url "https://github.com/changja88/homebrew-dotsync/archive/refs/tags/v0.1.27.tar.gz"
   sha256 "0000000000000000000000000000000000000000000000000000000000000000"
   license "MIT"
 
@@ -46,6 +46,6 @@ class Dotsync < Formula
   end
 
   test do
-    assert_match "dotsync 0.1.26", shell_output("#{bin}/dotsync --version")
+    assert_match "dotsync 0.1.27", shell_output("#{bin}/dotsync --version")
   end
 end
